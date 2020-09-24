9067e6a6b279e9fbcd28a92bd8219a6cdbd79051
tfltest <jiten.panchal.uk@gmail.com>
2020-09-24 18:40:58 +0100

    really

--- collections_practice/src/main/java/MicroserviceTest.java
+++ collections_practice/src/main/java/MicroserviceTest.java
@@ -1,77 +1,50 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
-
 
 public class MicroserviceTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	Map<String, UserStats> map1 = new HashMap<String, UserStats>();
+		Map<String, UserStats> map1 = new HashMap<String, UserStats>();
+
+		map1.put("1", new UserStats(Optional.of(3L)));
 		
-		UserStats user1 = new UserStats(Optional.of(1L));
-		
-		UserStats user2 = new UserStats(Optional.of(2L));
-		
-		
-		map1.put("1", new UserStats(Optional.of(1L)));
-		map1.put("3", new UserStats(Optional.of(2L)));
 		map1.put("2", new UserStats(Optional.ofNullable(null)));
-		map1.put("233", new UserStats(Optional.ofNullable(null)));
 
-//		map1.put(null, new UserStats(Optional.of(1L)));
-		
 		Map<String, UserStats> map2 = new HashMap<String, UserStats>();
+		map2.put("1", new UserStats(Optional.of(4L)));
 
-		System.out.println(count(map1,map2));
+		System.out.println(count(map1, map2));
 	}
-	
+
 	static Map<Long, Long> count(Map<String, UserStats>... visits) {
 		Map<Long, Long> result = new HashMap<Long, Long>();
 
 		if (visits == null)
 			return result;
-		
-		Stream.of(visits).flatMap(it -> Stream.of(it)).forEach(it -> System.out.println(it));
-		
-		
-		for (Map<String, UserStats> mapItem : visits) {
 
-			if (mapItem == null)
-				continue;
+		Stream.of(visits).forEach(mapItem -> {
+			mapItem.forEach((id, userStat) -> {
 
-			mapItem.keySet().removeIf(item -> {
+				Long serviceId = 0L;
 				try {
-					Long.parseLong(item);
-					return false;
+					serviceId = Long.parseLong(id);
 				} catch (NumberFormatException e) {
-					return true;
-				}
-			});
-			mapItem.values().removeIf(item -> {
-				return (item == null ||  !item.getVisitCount().isPresent() || item.getVisitCount().get() == null);
-			});
-		}
-
-		for (Map<String, UserStats> mapItem : visits) {
-			for (Map.Entry<String, UserStats> entry : mapItem.entrySet()) {
-
-				UserStats userStat = entry.getValue();
-				Long userId = Long.parseLong(String.valueOf(userStat.hashCode()));
-
-				if (!result.containsKey(userId)) {
-					result.put(userId, userStat.getVisitCount().orElse(0L));
-				} else {
-					result.put(userId, userStat.getVisitCount().get() + result.get(userId));
+					return;
 				}
 
-			}
-		}
+				if (userStat == null || !userStat.getVisitCount().isPresent()
+						|| userStat.getVisitCount().get() == null) {
+					return;
+				}
+
+				result.putIfAbsent(serviceId, 0L);
+				result.computeIfPresent(serviceId, (w, prev) -> prev + userStat.getVisitCount().get());
+			});
+		});
 
 		return result;
 	}
